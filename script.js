dc60406f275646ab594a347089a9e7c65c15e006
indrajeet-mrj <150013059+indrajeet-mrj@users.noreply.github.com>
2024-02-27 09:28:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const header=document.querySelector(".link");
+const nev_icon=document.querySelector(".nev-icon");
+nev_icon.addEventListener('click',()=>{
+  header.classList.toggle("header-res");
+});
